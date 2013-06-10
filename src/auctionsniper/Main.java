5e845673b592a51922e95f94f83ace51f9d6cbbd
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2013-06-10 18:35:19 +0100

    Chapter 11, test failure 2, cannot find label called "sniper status"

--- src/auctionsniper/Main.java
+++ src/auctionsniper/Main.java
@@ -1,13 +1,29 @@
 package auctionsniper;
 
+import auctionsniper.ui.MainWindow;
+
+import javax.swing.*;
+
 public class Main {
   public static final String AUCTION_RESOURCE = "Auction";
   public static final String ITEM_ID_AS_LOGIN = "auction-%s";
 
+
+  private MainWindow ui;
+
   public Main() throws Exception {
+    startUserInterface();
   }
 
   public static void main(String... args) throws Exception {
     Main main = new Main();
+  }
+
+  private void startUserInterface() throws Exception {
+    SwingUtilities.invokeAndWait(new Runnable() {
+      public void run() {
+        ui = new MainWindow();
+      }
+    });
   }
 }
