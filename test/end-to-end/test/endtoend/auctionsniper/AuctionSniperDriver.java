ce8c5dac117c4fb4d91395d7125c82fabb6a8c1e
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2013-06-10 11:38:10 +0100

    Full set of initial files

---
+++ test/end-to-end/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -0,0 +1,26 @@
+package test.endtoend.auctionsniper;
+
+import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.JFrameDriver;
+import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.gesture.GesturePerformer;
+
+import static auctionsniper.ui.MainWindow.MAIN_WINDOW_NAME;
+import static auctionsniper.ui.MainWindow.SNIPER_STATUS_NAME;
+import static org.hamcrest.Matchers.equalTo;
+
+@SuppressWarnings("unchecked")
+public class AuctionSniperDriver extends JFrameDriver {
+  public AuctionSniperDriver(int timeoutMillis) {
+    super(new GesturePerformer(),
+          JFrameDriver.topLevelFrame(
+            named(MAIN_WINDOW_NAME),
+            showingOnScreen()),
+            new AWTEventQueueProber(timeoutMillis, 100));
+  }
+
+  public void showsSniperStatus(String statusText) {
+    new JLabelDriver(
+        this, named(SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
+  }
+}
