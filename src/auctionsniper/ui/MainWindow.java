e78786dbaa68d2e778fe1e57d76975b448ffd412
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2013-06-10 18:35:19 +0100

    Chapter 11, test failure 2, cannot find label called "sniper status"

--- src/auctionsniper/ui/MainWindow.java
+++ src/auctionsniper/ui/MainWindow.java
@@ -7,4 +7,11 @@ public class MainWindow extends JFrame {
   public static final String SNIPER_STATUS_NAME = "sniper status";
   public static final String STATUS_JOINING = "Joining";
   public static final String STATUS_LOST = "Lost";
+
+  public MainWindow() {
+    super("Auction Sniper");
+    setName(MAIN_WINDOW_NAME);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
+  }
 }
