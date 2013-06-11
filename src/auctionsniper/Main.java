13beaf46db78d6d3ffeeade72951e3e82da514cf
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2013-06-11 18:42:23 +0100

    Chapter 12, test failure 1, sniper join message does not match

--- src/auctionsniper/Main.java
+++ src/auctionsniper/Main.java
@@ -20,7 +20,8 @@ public class Main {
   public static final String AUCTION_RESOURCE = "Auction";
   public static final String ITEM_ID_AS_LOGIN = "auction-%s";
   public static final String AUCTION_ID_FORMAT = ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE;
-
+  public static final String BID_COMMAND_FORMAT = "SOLVersion: 1.1; Command: BID; Price: %d;";
+  public static final String JOIN_COMMAND_FORMAT = "SOLVersion: 1.1; Command: JOIN;";
 
   private MainWindow ui;
   @SuppressWarnings("unused") private Chat notToBeGCd;
