3a8c0557fe13de3dda2f5c707aab6625da1fdab1
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2013-06-19 23:01:59 +0100

    Chapter 13, test failure 1, SniperListener.sniperLost not invoked

---
+++ src/auctionsniper/AuctionSniper.java
@@ -0,0 +1,11 @@
+package auctionsniper;
+
+public class AuctionSniper {
+  public AuctionSniper(SniperListener sniperListener) {
+    //TODO: Auto-generated
+  }
+
+  public void auctionClosed() {
+    //TODO: Auto-generated
+  }
+}
