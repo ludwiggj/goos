ce6277bb13a6f19d89f9273f326a777118703ed6
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2013-06-23 17:01:14 +0100

    Chapter_14, test failure 2, price message not received from sniper

--- test/unit/test/auctionsniper/AuctionMessageTranslatorTest.java
+++ test/unit/test/auctionsniper/AuctionMessageTranslatorTest.java
@@ -10,11 +10,14 @@ import org.jmock.integration.junit4.JMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static auctionsniper.AuctionEventListener.PriceSource;
+import static auctionsniper.AuctionSniper.SNIPER_ID;
+
 @RunWith(JMock.class)
 public class AuctionMessageTranslatorTest {
   private final Mockery context = new Mockery();
   private final AuctionEventListener listener = context.mock(AuctionEventListener.class);
-  private final AuctionMessageTranslator translator = new AuctionMessageTranslator(listener);
+  private final AuctionMessageTranslator translator = new AuctionMessageTranslator(SNIPER_ID, listener);
   public static final Chat UNUSED_CHAT = null;
 
   @Test
@@ -30,13 +33,25 @@ public class AuctionMessageTranslatorTest {
   }
 
   @Test
-  public void notifiesBidDetailWhenCurrentPriceMessageReceived() {
+  public void notifiesBidDetailsWhenCurrentPriceMessageReceivedFromOtherBidder() {
     context.checking(new Expectations() {{
-      exactly(1).of(listener).currentPrice(192, 7);
+      exactly(1).of(listener).currentPrice(192, 7, PriceSource.FromOtherBidder);
     }});
 
     Message message = new Message();
     message.setBody("SOLVersion: 1.1; Event: PRICE; CurrentPrice: 192; Increment: 7; Bidder: Someone else;");
+
+    translator.processMessage(UNUSED_CHAT, message);
+  }
+
+  @Test
+  public void notifiesBidDetailsWhenCurrentPriceMessageReceivedFromSniper() {
+    context.checking(new Expectations() {{
+      exactly(1).of(listener).currentPrice(234, 5, PriceSource.FromSniper);
+    }});
+
+    Message message = new Message();
+    message.setBody("SOLVersion: 1.1; Event: PRICE; CurrentPrice: 234; Increment: 5; Bidder: " + SNIPER_ID + ";");
 
     translator.processMessage(UNUSED_CHAT, message);
   }
